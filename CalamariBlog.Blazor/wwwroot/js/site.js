e1d71b1f0d8ecad642eff68fc2fabe106a2ae50f
Adrian Brink <adrian.brink@outlook.com>
2019-12-28 13:55:01 +0200

    minor update

--- CalamariBlog.Blazor/wwwroot/js/site.js
+++ CalamariBlog.Blazor/wwwroot/js/site.js
@@ -15,9 +15,21 @@
 }
 
 function SetMetaTags(model) {
+    var placeholderElement = $("#metaPlaceHolder");
+
+    placeholderElement.append(
+        '<title id="title" class="metaTags">Calamari Blog</title>' +
+        '<meta property="og:title" content="" id="meta_Title" class="metaTags" />' +
+        '<meta property="og:type" content="" id="meta_Type" class="metaTags" />' +
+        '<meta property="og:url" content="" id="meta_Url" class="metaTags" />' +
+        '<meta property="og:image" content="" id="meta_Image" class="metaTags" />'
+    );
+
     if (model.title) {
-        $("#title").text(model.title);
-        $("#meta_Title").attr('content', model.title);
+        placeholderElement.append(
+            '<title id="title" class="metaTags">' + model.title + '</title>' +
+            '<meta property="og:title" content="" id="meta_Title" class="metaTags" />' +
+        );
     }
     if (model.type) {
         $("#meta_Type").attr('content', model.type);
