e0dede64d5cd3a1279163c18edd7add72763dbac
Adrian Brink <adrian.brink@outlook.com>
2020-01-11 08:30:16 +0200

    cleanup

--- CalamariBlog.Blazor/gulpfile.js
+++ CalamariBlog.Blazor/gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('build-scripts-framework', function () {
         .src([
             './node_modules/jquery/dist/jquery.min.js',
             './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-            './wwwroot/js/clean-blog/clean-blog.min.js'
+            './node_modules/startbootstrap-clean-blog/js/clean-blog.min.js'
         ])
         .pipe(concat('framework.min.js'))
         .pipe(gulp.dest('./wwwroot/js'));
